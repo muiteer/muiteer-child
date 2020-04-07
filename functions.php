a9fb4de739edce157e50e62decb7579a33daabff
Muiteer <mail@muiteer.com>
2020-04-07 11:07:40 +0800

    Fixed child theme bugs.

--- functions.php
+++ functions.php
@@ -5,13 +5,13 @@
 
 function muiteer_enqueue_child_styles() {
   $parent_style = 'muiteer-parent-style';
-  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+  wp_enqueue_style($parent_style, get_template_directory_uri() . '/assets/css/style.css');
 
 }
-add_action( 'wp_enqueue_scripts', 'muiteer_enqueue_child_styles' );
+add_action('wp_enqueue_scripts', 'muiteer_enqueue_child_styles');
 
 function muiteer_enqueue_child_scripts() {
 	$parent_script = 'muiteer-parent-script';
-    wp_enqueue_script( $parent_script, get_stylesheet_directory_uri() . '/main.js', array( 'jquery' ),'',true );
+    wp_enqueue_script($parent_script, get_stylesheet_directory_uri() . '/assets/js/main.js', array('jquery'),'', true);
 }
-add_action( 'wp_enqueue_scripts', 'muiteer_enqueue_child_scripts' );
+add_action('wp_enqueue_scripts', 'muiteer_enqueue_child_scripts');
