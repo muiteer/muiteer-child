b302b1ed8d4d1ff92d2c77efee122e66f16681e2
Muiteer <mail@muiteer.com>
2020-06-22 11:54:54 +0800

    Changed the theme name: Muiteer.

--- functions.php
+++ functions.php
@@ -3,15 +3,15 @@
 /* This file should contain all the functions of the child theme. Most of the theme's functions can be overwritten (some are critical and shouldn't be tampered with). 
 */
 
-function zaxu_enqueue_child_styles() {
-  $parent_style = 'zaxu-parent-style';
+function muiteer_enqueue_child_styles() {
+  $parent_style = 'muiteer-parent-style';
   wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
 
 }
-add_action('wp_enqueue_scripts', 'zaxu_enqueue_child_styles');
+add_action('wp_enqueue_scripts', 'muiteer_enqueue_child_styles');
 
-function zaxu_enqueue_child_scripts() {
-	$parent_script = 'zaxu-parent-script';
+function muiteer_enqueue_child_scripts() {
+	$parent_script = 'muiteer-parent-script';
     wp_enqueue_script($parent_script, get_stylesheet_directory_uri() . '/main.js', array('jquery'), '', true);
 }
-add_action('wp_enqueue_scripts', 'zaxu_enqueue_child_scripts');
+add_action('wp_enqueue_scripts', 'muiteer_enqueue_child_scripts');
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 "use strict";
 
-function zaxuCallBack() {
+function muiteerCallBack() {
 	// Theme customization starts here
 }
