f21bfa69a639ed10fa74a3d4e456e6bd74b2f083
Muiteer <mail@muiteer.com>
2021-02-26 17:20:18 +0800

    Released V1.0.7.

--- functions.php
+++ functions.php
@@ -1,22 +1,29 @@
 <?php
 /*
  * @Description: Theme child functions
- * @Version: 1.0.6
+ * @Version: 1.0.7
  * @Author: ZAXU
  * @Link: http://www.zaxu.com
  * @Package: ZAXU
- */ 
+ */
+
+if ( !defined('ABSPATH') ) exit;
 
 function zaxu_enqueue_child_styles() {
   $parent_style = 'zaxu-parent-style';
   wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
-
 }
 add_action('wp_enqueue_scripts', 'zaxu_enqueue_child_styles');
 
 function zaxu_enqueue_child_scripts() {
   $parent_script = 'zaxu-parent-script';
-    wp_enqueue_script($parent_script, get_stylesheet_directory_uri() . '/main.js', array('jquery'), '', true);
+  wp_enqueue_script(
+    $parent_script,
+    get_stylesheet_directory_uri() . '/main.js',
+    array('jquery'),
+    '',
+    true
+  );
 }
 add_action('wp_enqueue_scripts', 'zaxu_enqueue_child_scripts');
 ?>
--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 "use strict";
-
 function zaxuCallBack() {
 	// Theme customization starts here
 }
